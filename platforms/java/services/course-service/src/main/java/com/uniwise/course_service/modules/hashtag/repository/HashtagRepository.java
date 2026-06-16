bbb207b68979843d90683260a0236530c3b11cbe
GoiToLaTri <dthanhtri1111@gmail.com>
2026-06-16 09:18:07 +0700

    refactor

--- platforms/java/services/course-service/src/main/java/com/uniwise/course_service/modules/hashtag/repository/HashtagRepository.java
+++ platforms/java/services/course-service/src/main/java/com/uniwise/course_service/modules/hashtag/repository/HashtagRepository.java
@@ -8,11 +8,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 
 import com.uniwise.course_service.modules.hashtag.entity.Hashtag;
 
-@Repository
 public interface HashtagRepository extends JpaRepository<Hashtag, String> {
  
     // ===== BASIC QUERIES =====
