a949bda1120834c9389ad2b9233ce427babdfa53
GoiToLaTri <dthanhtri1111@gmail.com>
2026-07-05 11:05:45 +0700

    refactor

--- platforms/java/services/payment-service/src/main/java/com/uniwise/payment_service/modules/payment/repository/PaymentRepository.java
+++ platforms/java/services/payment-service/src/main/java/com/uniwise/payment_service/modules/payment/repository/PaymentRepository.java
@@ -4,11 +4,9 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.uniwise.payment_service.modules.payment.entity.Payment;
 
-@Repository
 public interface PaymentRepository extends JpaRepository<Payment, String> {
     Optional<Payment> findByTxnRef(String txnRef);
     List<Payment> findByAccountId(String accountId);
